4117a4cab7fa7c9ffddb7f3a05f5b450dd9888f1
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-09-13 15:30:54 -0700

    task_1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     nouvelle_list = list(map(lambda i: replace if i == search else i, my_list))
-    return (new_list)
+    return (nouvelle_list)
