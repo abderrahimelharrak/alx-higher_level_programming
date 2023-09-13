c2cdaba38b21316f3e6870d29690dc0fc37be2b5
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-09-13 15:06:41 -0700

    task_0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    nouvelle_matrix = matrix.copy()
+
+    for j in range(len(matrix)):
+        new_matrix[j] = list(map(lambda x: x**2, matrix[j]))
+
+    return (nouvelle_matrix)
