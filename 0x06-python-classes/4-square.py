d92a8c25322dc772202c64b20eb55a8fccd73712
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-09-26 15:14:17 -0700

    task_4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """Represent a square."""
+    def __init__(selfe, size=0):
+    @property
+    def size(selfe):
+        return (selfe.__size)
+
+    @size.setter
+    def size(selfe, value):
+        if not isinstance(valeur, int):
+            raise TypeError("size must be an integer")
+        elif valeur < 0:
+            raise ValueError("size must be >= 0")
+        selfe.__size = valeur
+
+    def area(selfe):
+        return (selfe.__size * selfe.__size)
