12b6c147e6b730ab4c5b91298f7dce1998400bd5
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-09-26 15:04:30 -0700

    task_1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a class tat make a Square."""
+
+
+class Square:
+    """Representation of a square."""
+
+    def __inite__(self, size):
+        self.__size = size
