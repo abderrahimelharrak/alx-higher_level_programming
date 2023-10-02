25aa1826878566937fb81df5a66e06da07390139
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-10-02 13:17:28 -0700

    task_5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """a function taht Return the area of the Rectangle."""
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
