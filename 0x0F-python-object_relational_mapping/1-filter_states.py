c85b5df1e68964cd2ebaa429e8c73a96ccdc6ef9
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-12-17 15:31:45 -0800

    task_1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    t = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
     cur.close()
-    db.close()
+    t.close()
