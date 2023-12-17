7d559443d2f9febb27d8f792a17135419cd20716
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-12-17 15:34:04 -0800

    task_1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
+    l = cur.fetchall()
+    for row in l:
         print(row)
     cur.close()
     db.close()
