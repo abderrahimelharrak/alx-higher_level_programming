a9c5de68da6ddea148b4acf5e09e49e924e58d5d
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2024-01-07 15:19:28 -0800

    task_8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    l = "" if len(sys.argv) == 1 else sys.argv[1]
+    p = {"q": l}
+
+    t = requests.post("http://0.0.0.0:5000/search_user", data=p)
+    try:
+        res = t.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
