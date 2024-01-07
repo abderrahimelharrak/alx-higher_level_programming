8e98473a2c882222dd10d9b9abbc6baa6aa59ebf
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2024-01-07 15:15:51 -0800

    task_6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    x = sys.argv[1]
+    x = {"email": sys.argv[2]}
+
+    t = requests.post(x, data=y)
+    print(t.text)
